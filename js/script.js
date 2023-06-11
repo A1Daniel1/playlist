f2b65844aa21684ed6ab0bc677a89e25441fab3f
Daniel <danielahumada071@gmail.com>
2023-06-11 16:21:17 -0500

    testing the javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("hello");
+console.log("works")
+
+const button = document.getElementById("button_1")
+
+button.addEventListener("click", hey)
+
+function hey(True) {
+    console.log("hey");
+}
